1dd9a72f6ef0f59c9090015d0e6834cdff2acdb9
felixpy <felixpy.1993@gmail.com>
2019-03-19 22:11:01 +0800

    docs: update nav config

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -63,12 +63,12 @@ module.exports = {
           { text: '指南', link: '/guide/' },
           { text: 'Yo Antd Pro', link: '/antd-pro/' },
           { text: 'Element Admin', link: '/element-admin/' },
+          { text: 'Form Builder', link: '/element-form-builder/' },
           { text: 'GOT Auth', link: '/gotauth/' },
-          { text: 'Wishtodo', link: '/wishtodo/' },
           {
             text: '更多',
             items: [
-              { text: 'Form Builder', link: '/element-form-builder/' },
+              { text: 'Wishtodo', link: '/wishtodo/' },
               { text: 'Egg Token', link: '/egg-token/' }
             ]
           }
@@ -87,12 +87,12 @@ module.exports = {
           { text: 'Guide', link: '/en/guide/' },
           { text: 'Yo Antd Pro', link: '/en/antd-pro/' },
           { text: 'Element Admin', link: '/en/element-admin/' },
+          { text: 'Form Builder', link: '/en/element-form-builder/' },
           { text: 'GOT Auth', link: '/en/gotauth/' },
-          { text: 'Wishtodo', link: '/en/wishtodo/' },
           {
             text: 'More',
             items: [
-              { text: 'Form Builder', link: '/en/element-form-builder/' },
+              { text: 'Wishtodo', link: '/en/wishtodo/' },
               { text: 'Egg Token', link: '/en/egg-token/' }
             ]
           }
